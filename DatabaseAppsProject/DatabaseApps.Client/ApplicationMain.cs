87a180899f832dd7ed1423f42fefe1b1aec09f25
Spas Vutov <VutovSpas@gmail.com>
2015-07-19 01:14:50 +0300

    MySql and SQLite extraction logic extracted in seperate class.

--- DatabaseAppsProject/DatabaseApps.Client/ApplicationMain.cs
+++ DatabaseAppsProject/DatabaseApps.Client/ApplicationMain.cs
@@ -1,11 +1,7 @@
 ﻿namespace DatabaseApps.Client
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using DbManagers;
     using Exporters;
-    using Models;
 
     public class ApplicationMain
     {
@@ -38,68 +34,8 @@
         /// </summary>
         private static void ExportToExcel()
         {
-            // SQLite
-            var sqliteManager = new SQLiteDBManager();
-            var sqliteProducts = sqliteManager.GetAllProducts();
-            // foreach (var product in sqliteProducts)
-            // {
-            //     Console.WriteLine("{0} {1}%", product.Key, product.Value);
-            // }
-            Console.WriteLine("SQLite data gathered.");
-
-            // MySQL
-            var mysqlManager = new MysqlDBManager();
-            // var mysqlVendors = mysqlManager.MySqlContext.Vendors.Count();
-            var mysqlVendors = mysqlManager.MySqlContext.Vendors
-                .Select(v => new
-                {
-                    VendorName = v.Name,
-                    Products = v.Products.Select(p => new
-                    {
-                        ProductName = p.Name,
-                        Income = p.Sales.Select(s => s.TotalIncome).Sum()
-                    }),
-                    Expense = v.Expenses.Select(e => e.Amount)
-                })
-                .ToList();
-            Console.WriteLine("MySQL data gathered.");
-
-            var excelReportDatas = new List<ExcelReportData>();
-
-            mysqlVendors.ForEach(v =>
-            {
-                var totalTaxes = 0m;
-                var totalIncome = 0m;
-                v.Products.ToList().ForEach(p =>
-                {
-                    var tax = 0m;
-                    if (sqliteProducts.ContainsKey(p.ProductName))
-                    {
-                        tax = (decimal)sqliteProducts[p.ProductName];
-                    }
-
-                    totalTaxes += p.Income * tax / 100;
-                    totalIncome += p.Income;
-                });
-
-                var financialResult = totalIncome - totalTaxes - v.Expense.Sum();
-
-                var data = new ExcelReportData()
-                {
-                    FinancialResult = financialResult,
-                    TotalExpense = v.Expense.Sum(),
-                    TotalIncome = totalIncome,
-                    TotalTaxes = totalTaxes,
-                    VendorName = v.VendorName
-                };
-
-                excelReportDatas.Add(data);
-            });
-
-            // excelReportDatas.ForEach(Console.WriteLine);
-            Console.WriteLine("Excel report issued.");
-
-            ExcelExporter.ExportToExcel(excelReportDatas);
+            var excelReportData = SQLiteAndMySQLExporter.GetReportDate();
+            ExcelExporter.ExportToExcel(excelReportData);
         }
     }
 }
---
+++ DatabaseAppsProject/DatabaseApps.Client/Exporters/SQLiteAndMySQLExporter.cs
@@ -0,0 +1,75 @@
+﻿namespace DatabaseApps.Client.Exporters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DbManagers;
+    using Models;
+
+    public static class SQLiteAndMySQLExporter
+    {
+        public static List<ExcelReportData> GetReportDate()
+        {
+            // SQLite
+            var sqliteManager = new SQLiteDBManager();
+            var sqliteProducts = sqliteManager.GetAllProducts();
+            // System.Console.WriteLine(sqliteProducts.Count());
+            Console.WriteLine("SQLite data gathered.");
+
+            // MySQL
+            var mysqlManager = new MysqlDBManager();
+            // var mysqlVendors = mysqlManager.MySqlContext.Vendors.Count();
+            var mysqlVendors = mysqlManager.MySqlContext.Vendors
+                .Select(v => new
+                {
+                    VendorName = v.Name,
+                    Products = v.Products.Select(p => new
+                    {
+                        ProductName = p.Name,
+                        Income = p.Sales.Select(s => s.TotalIncome).Sum()
+                    }),
+                    Expense = v.Expenses.Select(e => e.Amount)
+                })
+                .ToList();
+            Console.WriteLine("MySQL data gathered.");
+
+            var excelReportData = new List<ExcelReportData>();
+
+            // Aggregation of data
+            mysqlVendors.ForEach(v =>
+            {
+                var totalTaxes = 0m;
+                var totalIncome = 0m;
+                v.Products.ToList().ForEach(p =>
+                {
+                    var tax = 0m;
+                    if (sqliteProducts.ContainsKey(p.ProductName))
+                    {
+                        tax = (decimal)sqliteProducts[p.ProductName];
+                    }
+
+                    totalTaxes += p.Income * tax / 100;
+                    totalIncome += p.Income;
+                });
+
+                var financialResult = totalIncome - totalTaxes - v.Expense.Sum();
+
+                var data = new ExcelReportData()
+                {
+                    FinancialResult = financialResult,
+                    TotalExpense = v.Expense.Sum(),
+                    TotalIncome = totalIncome,
+                    TotalTaxes = totalTaxes,
+                    VendorName = v.VendorName
+                };
+
+                excelReportData.Add(data);
+            });
+
+            // excelReportDatas.ForEach(Console.WriteLine);
+            Console.WriteLine("Excel report issued.");
+
+            return excelReportData;
+        } 
+    }
+}
